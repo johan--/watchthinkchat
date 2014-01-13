2dfc1bbfa5dfc4771aeb419abcbc0ccc1b724369
Andrew Roth <andrewroth@gmail.com>
2014-01-13 12:58:26 -0500

    don't bother with a uid on create

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,17 +1,9 @@
 class Campaign < ActiveRecord::Base
   validates :name, :missionhub_secret, presence: true
-	before_create :generate_uid
 
   def as_json(options = {})
     #super({ :only => [ :title, :type, :permalink ] }.merge(options))
     { :title => self.name, :type => "", :permalink => self.permalink }
   end
 
-	private
-
-	def generate_uid
-		begin
-			self.uid = SecureRandom.hex(3)
-		end while Campaign.exists?(uid: uid)
-	end
 end
