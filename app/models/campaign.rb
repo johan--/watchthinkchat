660698c478006658640849450efc110222583188
Andrew Roth <andrewroth@gmail.com>
2014-01-16 16:58:46 -0500

    add uid to json output

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,7 +4,7 @@ class Campaign < ActiveRecord::Base
 
   def as_json(options = {})
     #super({ :only => [ :title, :type, :permalink ] }.merge(options))
-    { :title => self.name, :type => self.campaign_type, :permalink => self.permalink, :video_id => self.video_id }
+    { :title => self.name, :type => self.campaign_type, :permalink => self.permalink, :video_id => self.video_id, :uid => self.uid }
   end
 
   protected
