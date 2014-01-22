082c115334c560257f149211e37b164cbbd91e59
Adam Meyer <adam.meyer@cru.org>
2014-01-22 17:19:12 -0500

    removed old js

--- app/assets/javascripts/operators/controllers/manager_controller.js
+++
@@ -1,3 +0,0 @@
-app.controller('ManagerController', function($scope, Campaign) {
-
-});
--- app/assets/javascripts/operators/controllers/message_controller.js
+++
@@ -1,8 +0,0 @@
- app.controller('MessageController', function($scope, socket) {
-  $scope.time = new Date();
-  // Methods published to the scope
-  // ==============================
-  $scope.capitaliseFirstLetter = function(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-});
