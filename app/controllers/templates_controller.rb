fbdd46ae7fe0901bbd9537bffd08884d25d2628c
Andrew Roth <andrewroth@gmail.com>
2014-01-23 14:01:15 -0500

    test a log line

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -2,6 +2,7 @@ class TemplatesController < ApplicationController
   before_filter :authenticate_by_facebook!, :if => Proc.new { |c| request.domain == ENV['operator_app_url'] }
 
   def index
+    logger.info "In TemplatesController"
     if request.domain == ENV['operator_app_url'] && signed_in? && !params[:o].present?
       session[:campaign] ||= params[:path]
       @campaign = Campaign.where("uid = ? OR permalink = ?", session[:campaign], session[:campaign]).first
