dcab294b16655abf88d28dec888fc7707a36fffe
Adam Meyer <adam.meyer@cru.org>
2014-02-14 15:52:13 -0500

    after chat flow

--- app/assets/javascripts/controllers/ChatController.js
+++ app/assets/javascripts/controllers/ChatController.js
@@ -51,34 +51,46 @@ angular.module('chatApp').controller('ChatController', function ($scope, $rootSc
       }
 
       $scope.followup_buttons.push({
-        text: 'No',
+        text: 'No thanks',
         action: 'url',
-        value: 'http://cru.org'
+        value: 'http://cru.org',
+        message_active_chat: 'We appreciate the time you gave to watch Falling Plates.Thanks for telling us straight up what you think. There is obviously a reason in your mind right now that causes you to say no to following Jesus. We would love to have a chat about it, if you are interested lets chat on your right hand side--->',
+        message_no_chat: 'Thanks for telling us straight up what you think. We really appreciate the time you gave to watch Falling Plates. There is obviously a reason in your mind right now that causes you to say no to following Jesus. '
       });
       $scope.followup_buttons.push({
-        text: 'I follow another religion',
+        text: 'I follow another faith',
         action: 'url',
-        value: 'http://gotcx.com/'
+        value: 'http://gotcx.com/',
+        message_active_chat: 'Thanks for taking time to watch #Fallingplates and considering how Jesus desires to bring life to you today. What is it that you find interesting about Jesus? Lets chat on your right hand side ----->',
+        message_no_chat: 'Thanks for taking time to watch #Fallingplates and considering how Jesus desires to bring life to you today. We would like to help you understand the uniqueness of Jesus and what we believe he has done for you. If you are interested we have an article here that you could look into and see if Jesus is really is ----> '
       });
       $scope.followup_buttons.push({
         text: 'I am not sure',
         action: 'chat',
-        value: ''
+        value: '',
+        message_active_chat: 'This is a big deal :) and we pumped that u want to take the time to at least consider following Jesus. What is one thing that is attractive to u in following Jesus? What is one thing that makes u hesitant? Love to chat with ya about this stuff in the chat panel on the right :)   ----->',
+        message_no_chat: 'This is an important issue and we are glad that you want to take the time to consider following Jesus. Regarding following Jesus, what is one thing that is attractive to you? What is one thing that makes you hesitant? We have a growth adventure that can help you find significance in following Jesus. Find out more "Take the growth adventure" ------>'
       });
       $scope.followup_buttons.push({
         text: 'I want to start',
         action: 'chat',
-        value: ''
+        value: '',
+        message_active_chat: 'Thanks for taking time to watch #FallingPlates and for considering Jesus’s call to follow Him. To desire to start following Jesus is a significant step! Tell us a bit about what’s behind your desire? Luv to chat with ya about this stuff in the chat panel on the right :)   ----->',
+        message_no_chat: 'Thanks for taking time to watch #FallingPlates and for considering Jesus’s call to follow Him. To want to begin to start following Jesus is a significant step. We have a growth adventure that can help u grow after u have asked Christ to come into your life. Heres the place for u to get connected with a friend to grow :) '
       });
       $scope.followup_buttons.push({
-        text: 'I am trying',
+        text: 'I\'m trying',
         action: 'chat',
-        value: ''
+        value: '',
+        message_active_chat: 'Thanks for watching #FallingPlates and taking time to express how u feel about following Jesus. Sometimes we feel like we will never get there, be good enough or be the person God wants us to be…….which can leave us feeling disappointed or disillusioned. Love to chat with ya about this stuff in the chat panel on the right :)   ----->',
+        message_no_chat: 'Thanks for watching #FallingPlates and taking time to express how u feel about following Jesus. Sometimes we feel like we will never get there, be good enough or be the person God wants us to be…….which can leave us feeling disappointed or disillusioned. We have a growth adventure that can help you find real satisfaction and confidence in following Jesus. It will help you understand that the relationship is based on his efforts which are sufficient :) To find out more click here ---->'
       });
       $scope.followup_buttons.push({
-        text: 'I am following you',
+        text: 'I\'m following you',
         action: 'chat',
-        value: ''
+        value: '',
+        message_active_chat: 'We are pumped that u are following Jesus! We would love to connect with u and discover how your journey is going and what you think the next steps are to be who He wants you to be? Lets chat over on your right-----> :)',
+        message_no_chat: 'We are pumped that u are following Jesus! We know that its not always an easy path, but it’s definitely worth it :)  How ever you feel you are doing today, we are glad He led you here! It’s our goal to help you grow and to help you have influence. We would love to connect you with someone in the adventure of knowing Jesus. You up for the adventure?'
       });
     }).error(function (data, status, headers, config) {
 
@@ -99,6 +111,9 @@ angular.module('chatApp').controller('ChatController', function ($scope, $rootSc
   console.log('operator: '+operator_data.uid,'visitor: '+visitor_data.uid);
 
   $scope.postMessage = function () {
+    if($scope.chatMessage==''){
+      return;
+    }
     var post_data = {
       user_uid: visitor_data.uid,
       message_type: 'user',
@@ -133,21 +148,27 @@ angular.module('chatApp').controller('ChatController', function ($scope, $rootSc
         operator_data = data.operator;
         console.log('================ New Chat Created: ' + data.chat_uid);
 
-        $('#chatbox').fadeIn();
+        $('#chatbox, #after-chat-information-01').fadeIn();
 
-        $('.conversation').append('<li>   <img src="' + operator_data.profile_pic + '" style="float:left; padding:6px;">   <div class="message">You are now chatting with<br>' + operator_data.name + '</div>      <div class="timestamp pull-right timestamp-refresh" timestamp="' + Math.round(+new Date()).toString() + '">Just Now</div>      <div class="person">-</div></li>');
-        console.log('Inital message: '+initialMsg);
+        $('#chatting_with').html('<img src="' + operator_data.profile_pic + '" style="float:left; padding:6px;">   <div class="message">You are chatting with<br>' + operator_data.name + '</div>');
+        //console.log('Inital message: '+initialMsg);
 
         var pusher = new Pusher('249ce47158b276f4d32b');
         pusher.subscribe('visitor_' + visitor_data.uid);
         var channel_chat = pusher.subscribe('chat_'+chat_data.chat_uid);
         channel_chat.bind('event', function (data) {
-          if(data.user_uid != visitor_data.uid){
-            $('.conversation').append('<li>      <div class="message">' + data.message + '</div>      <div class="timestamp pull-right timestamp-refresh" timestamp="' + Math.round(+new Date()).toString() + '">Just Now</div>      <div class="person">' + operator_data.name + '</div></li>');
-            $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
+          if (data.user_uid != visitor_data.uid) {
+            if (data.message_type == 'activity') {
+              if(data.message == 'challenge'){
+                $('.after-chat-challenge').fadeIn();
+              }
+            } else {
+              $('.conversation').append('<li>      <div class="message">' + data.message + '</div>      <div class="timestamp pull-right timestamp-refresh" timestamp="' + Math.round(+new Date()).toString() + '">Just Now</div>      <div class="person">' + operator_data.name + '</div></li>');
+              $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
 
-            if(window_focus === false){
-              $('#newMsgAlert')[0].play();
+              if (!window_focus) {
+                $('#newMsgAlert')[0].play();
+              }
             }
           }
         });
@@ -184,6 +205,29 @@ angular.module('chatApp').controller('ChatController', function ($scope, $rootSc
       }else{
         postActivityMessage('Visitor has clicked: ' + button.text);
       }
+      $scope.after_chat_message = button.message_active_chat;
+    }
+  };
+
+  $scope.nextStep = function(step){
+    if(step == 2){
+      $('#after-chat-information-01, .after-chat-challenge').hide();
+      $('#after-chat-information-02').show();
+    }else if(step == 3){
+      $('#after-chat-information-02').hide();
+      $('#after-chat-information-03').show();
+    }else if(step == 4){
+      FB.ui({
+        method: 'send',
+        link: 'http://www.godchat.tv'
+      });
+      $scope.nextStep(7);
+    }else if(step == 5){
+      $('#after-chat-information-03').hide();
+      $('#after-chat-information-05').show();
+    }else if(step == 7){
+      $('#after-chat-information-06').show();
+      $('#after-chat-information-03, #after-chat-information-05').hide();
     }
   };
 
@@ -241,6 +285,12 @@ angular.module('chatApp').controller('ChatController', function ($scope, $rootSc
     window_focus=true;
   }).blur(function() {
     window_focus=false;
+  });
+
+  FB.init({
+    appId      : '555591577865154',
+    status     : true,
+    xfbml      : true
   });
 
 });
