61758afe05ff063349743ceea28a8deeb2565c40
Andrew Roth <andrewroth@gmail.com>
2014-02-19 11:02:31 -0500

    ok apparently there was no followup_buttons table

--- db/migrate/20140219160028_create_followup_buttons.rb
+++ db/migrate/20140219160028_create_followup_buttons.rb
@@ -1,10 +1,14 @@
 class CreateFollowupButtons < ActiveRecord::Migration
   def change
     create_table :followup_buttons do |t|
-      t.integer :chat_id
       t.string :btn_text
       t.string :btn_action
       t.string :btn_value
+      t.integer :campaign_id
+      t.text :message_active_chat
+      t.text :message_no_chat
+
+      t.timestamps
     end
   end
 end
