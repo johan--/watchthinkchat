dc97c24bf06683e86e2ac5f94b4049c92fad466f
Andrew Roth <andrewroth@gmail.com>
2014-02-20 18:07:37 -0500

    actually store the internet db id in chat.operator_whose_link_id
    but make sure to send out params[:operator_uid] in the pusher message

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -5,18 +5,19 @@ class Api::ChatsController < ApplicationController
     campaign = Campaign.where(:uid => params[:campaign_uid]).first
     visitor = User.where(:visitor_uid => params[:visitor_uid]).first
     operator = User.where(:operator_uid => params[:operator_uid]).first
+    requested_operator = User.where(:operator_uid => params[:operator_uid]).first
     if campaign.max_chats && operator.count_operator_open_chats_for(campaign) >= campaign.max_chats
       operator = campaign.get_available_operator
     end
 
     if campaign && visitor && operator && operator.online?
-      chat = Chat.create!(:campaign_id => campaign.id, :visitor_id => visitor.id, :operator_id => operator.id, :operator_whose_link_id => operator.id)
+      chat = Chat.create!(:campaign_id => campaign.id, :visitor_id => visitor.id, :operator_id => operator.id, :operator_whose_link_id => requested_operator.id)
       Pusher["operator_#{operator.operator_uid}"].trigger('newchat', {
         chat_uid: chat.uid,
         visitor_uid: visitor.visitor_uid,
         visitor_name: visitor.fullname,
         visitor_profile: "",
-        requested_operator: operator.operator_uid
+        requested_operator: params[:operator_uid]
       })
       render json: { chat_uid: chat.uid, operator: operator.as_json(:as => :operator) }, status: 201
     elsif !operator
