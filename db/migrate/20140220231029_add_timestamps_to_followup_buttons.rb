9563ecf9c9b7bb685e7d2e95f1aca5a0fb048a7d
Andrew Roth <andrewroth@gmail.com>
2014-02-20 18:43:13 -0500

    I like this better

--- db/migrate/20140220231029_add_timestamps_to_followup_buttons.rb
+++ db/migrate/20140220231029_add_timestamps_to_followup_buttons.rb
@@ -1,9 +1,6 @@
 class AddTimestampsToFollowupButtons < ActiveRecord::Migration
   def change
-    begin
-      #add_column(:followup_buttons, :created_at, :datetime)
-      #add_column(:followup_buttons, :updated_at, :datetime)
-    rescue
-    end
+    add_column(:followup_buttons, :created_at, :datetime) unless FollowupButton.column_names.include?("created_at")
+    add_column(:followup_buttons, :updated_at, :datetime) unless FollowupButton.column_names.include?("updated_at")
   end
 end
