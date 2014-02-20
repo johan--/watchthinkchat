e0abeebc196735c4a9010e5dc9275b432988caca
Andrew Roth <andrewroth@gmail.com>
2014-02-20 18:39:52 -0500

    even a migration with begin end still crashes

--- db/migrate/20140220231029_add_timestamps_to_followup_buttons.rb
+++ db/migrate/20140220231029_add_timestamps_to_followup_buttons.rb
@@ -1,8 +1,8 @@
 class AddTimestampsToFollowupButtons < ActiveRecord::Migration
   def change
     begin
-      add_column(:followup_buttons, :created_at, :datetime)
-      add_column(:followup_buttons, :updated_at, :datetime)
+      #add_column(:followup_buttons, :created_at, :datetime)
+      #add_column(:followup_buttons, :updated_at, :datetime)
     rescue
     end
   end
