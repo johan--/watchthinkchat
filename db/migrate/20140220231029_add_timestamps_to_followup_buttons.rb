b3be2454d09ac6474e624d868dd8f065d5d1be48
Andrew Roth <andrewroth@gmail.com>
2014-02-20 18:24:26 -0500

    seems production already has created_at columns on followup_buttons

--- db/migrate/20140220231029_add_timestamps_to_followup_buttons.rb
+++ db/migrate/20140220231029_add_timestamps_to_followup_buttons.rb
@@ -1,6 +1,9 @@
 class AddTimestampsToFollowupButtons < ActiveRecord::Migration
   def change
-    add_column(:followup_buttons, :created_at, :datetime)
-    add_column(:followup_buttons, :updated_at, :datetime)
+    begin
+      add_column(:followup_buttons, :created_at, :datetime)
+      add_column(:followup_buttons, :updated_at, :datetime)
+    rescue
+    end
   end
 end
