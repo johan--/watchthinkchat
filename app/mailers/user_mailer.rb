307f6cdb6f2b204cf21dfa7db31544f36432b69f
Andrew Roth <andrewroth@gmail.com>
2014-02-27 09:45:25 -0500

    fix message still not working right

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,12 @@ class UserMailer < ActionMailer::Base
   
   def email(params)
     if params[:from_name].present?
-      from = "noreply@cru.org <#{params[:from]}>"
+      from = "noreply@cru.org <#{params[:from_name]}>"
+    else
+      from = "noreply@cru.org"
     end
-    mail(to: params[:to], from: from, message: params[:message].html_safe, reply_to: params[:from], subject: params[:subject], content_type: 'text/plain') do |format|
-      format.text
+    mail(to: params[:to], from: from, reply_to: params[:from], subject: params[:subject], content_type: 'text/plain') do |format|
+      format.text { render text: params[:message] }
     end
   end
 end
