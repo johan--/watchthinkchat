9e63cce81a4d922a19c81726169a4310da37a271
Andrew Roth <andrewroth@gmail.com>
2014-02-27 00:06:33 -0500

    keep the current email if it exists and the new one submitted from the operator isn't present

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -27,6 +27,7 @@ class Chat < ActiveRecord::Base
 
   def collect_stats(params)
     name_words = params[:visitor_name].split(' ')
+    params.delete(:visitor_email) unless params[:visitor_email].present?
     visitor.update_attributes :first_name => name_words.shift, :last_name => name_words.join(" "), :email => params[:visitor_email], :assigned_operator1_id => operator_id, :assigned_operator2_id => operator_whose_link_id
     visitor.sync_mh
     r = Rest.post("https://www.missionhub.com/apis/v3/followup_comments?secret=#{campaign.missionhub_token}&followup_comment[contact_id]=#{visitor.missionhub_id}&followup_comment[commenter_id]=#{self.operator.missionhub_id}&followup_comment[comment]=#{build_comment(params)}")
