2963b02347764bf1ad049e7bad3d9d4ba711b83a
Andrew Roth <andrewroth@gmail.com>
2014-02-27 09:16:07 -0500

    last commit didn't work, try a few more to get text-only email to work properly

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
     if params[:from_name].present?
       from = "noreply@cru.org <#{params[:from]}>"
     end
-    mail(to: params[:to], from: from, message: params[:message], reply_to: params[:from], subject: params[:subject]) do |format|
+    mail(to: params[:to], from: from, message: params[:message].html_safe, reply_to: params[:from], subject: params[:subject], content_type: 'text/plain') do |format|
       format.text
     end
   end
