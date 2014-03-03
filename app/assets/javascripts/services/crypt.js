619f9cc7b1c1e62070f8a1fdee37b66375c71af2
adammeyer <adam.meyer@cru.org>
2014-03-03 14:30:36 -0500

    mobile css bug fixes

---
+++ app/assets/javascripts/services/crypt.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('chatApp')
+    .service('Crypt', function () {
+        var key = "SXdpkoWLZPDOKFIVUHJYTQBifuvjhtybqmGNMACERxswgzlncare";
+        this.encodeStr = function (uncoded) {
+            uncoded = uncoded.toUpperCase().replace(/^\s+|\s+$/g, "");
+            var coded = "";
+            var chr;
+            for (var i = uncoded.length - 1; i >= 0; i--) {
+                chr = uncoded.charCodeAt(i);
+                coded += (chr >= 65 && chr <= 90) ?
+                    key.charAt(chr - 65 + 26 * Math.floor(Math.random() * 2)) :
+                    String.fromCharCode(chr);
+            }
+            return encodeURIComponent(coded);
+        }
+
+        this.decodeStr = function (coded) {
+            coded = decodeURIComponent(coded);
+            var uncoded = "";
+            var chr;
+            for (var i = coded.length - 1; i >= 0; i--) {
+                chr = coded.charAt(i);
+                uncoded += (chr >= "a" && chr <= "z" || chr >= "A" && chr <= "Z") ?
+                    String.fromCharCode(65 + key.indexOf(chr) % 26) :
+                    chr;
+            }
+            return uncoded;
+        }
+    });
---
+++ app/assets/javascripts/services/timeUpdate.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('chatApp')
+    .service('timeUpdate', function () {
+        var timeUpdate = setInterval(function () {
+            $('.conversation .timestamp-refresh').each(function (i) {
+                var origtime = parseInt($(this).attr('timestamp'));
+                var unixtime = Math.round(+new Date());
+
+                if ((unixtime - origtime) < 10000) {
+                    $(this).html('Just Now');
+                } else if ((unixtime - origtime) < 60000) {
+                    $(this).html('about ' + Math.round((unixtime - origtime)/1000).toString() + ' seconds ago');
+                } else if ((unixtime - origtime) < 120000) {
+                    $(this).html('about 1 min ago');
+                } else if ((unixtime - origtime) < 240000) {
+                    $(this).html('about ' + Math.floor(((unixtime - origtime) / 60000)).toString() + ' mins ago');
+                }else{
+                    $(this).html($filter('date')(origtime, 'shortTime'));
+                    $(this).removeClass('timestamp-refresh');
+                }
+            });
+        }, 5000);
+    });
