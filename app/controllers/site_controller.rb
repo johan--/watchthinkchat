095df160544da842711e79b56b19da0dd2608348
Andrew Roth <andrewroth@gmail.com>
2014-03-05 09:25:37 -0500

    only redirect the visitor root site to fallingplates

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,8 @@ class SiteController < ApplicationController
   def index
     logger.info "Session: #{session.inspect}"
     @campaign = Campaign.where(:permalink => "fallingplates").first
-    redirect_to "/c/#{@campaign.uid}"
+    if request.host == ENV['visitor_app_url']
+      redirect_to "/c/#{@campaign.uid}"
+    end
   end
 end
