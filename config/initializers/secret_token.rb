d1ed74fa62a527357747d455a512275f3ee7ac07
Andrew Roth <andrewroth@gmail.com>
2014-03-06 00:55:17 -0500

    use secret from env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Godchat::Application.config.secret_token = '4c654c0c2e4e4c9831f8044368ebd05c61658411e289c45d67a9055b0148dc39efffa804953e6416a74a3f5ac985a8f82980ec7243d4c9431256b4bbac47f3bf'
+Godchat::Application.config.secret_token = ENV['secret']
