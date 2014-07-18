a0d95efe72088e906dd79af524c8279c64070e1d
adammeyer <adam.meyer@cru.org>
2014-07-18 17:13:30 -0400

    Start of campaign manager

--- app/assets/javascripts/controllers/ManageEditController.js
+++ app/assets/javascripts/controllers/ManageEditController.js
@@ -1,28 +1,73 @@
-angular.module('chatApp').controller('ManageEditController', function ($scope, $routeParams, manageApi) {
+angular.module('chatApp').controller('ManageEditController', function ($scope, $routeParams, $timeout, manageApi) {
+  var activeCampaignIndex;
   manageApi.call('get', 'campaigns', {}, function(data){
     $scope.myCampaigns = data;
     if(angular.isDefined($routeParams.campaignId)){
       $scope.activeCampaign = angular.copy(_.find(data, { 'uid': $routeParams.campaignId }));
+      activeCampaignIndex = _.findIndex(data, { 'uid': $routeParams.campaignId });
+
+      $scope.refreshStats();
     }else{
       //new campaign defaults
       $scope.activeCampaign = {
         growth_challenge: 'auto',
         type: 'youtube',
         max_chats: 4,
-        language: 'en'
+        language: 'en',
+        status: 'opened'
       };
     }
   }, null, true);
 
+  $scope.refreshStats = function () {
+    $scope.statsUpdateTime = '-';
+    manageApi.call('get', 'campaigns/' + $routeParams.campaignId + '/stats', {}, function(data){
+      $scope.campaignStats = data;
+      $scope.statsUpdateTime = new Date();
+    });
+  };
+
   $scope.saveCampaign = function () {
-    manageApi.call('put', 'campaigns/' + $scope.activeCampaign.uid + '.json', $scope.activeCampaign, function(data){
-      console.log(data);
+    $scope.notify = {
+      message: 'Saving...',
+      class: 'bg-warning'
+    };
+    manageApi.call('put', 'campaigns/' + $scope.activeCampaign.uid, $scope.activeCampaign, function(data){
+      $scope.notify = {
+        message: 'Campaign saved.',
+        class: 'bg-success'
+      };
+      $scope.myCampaigns[activeCampaignIndex] = data;
+      console.log(activeCampaignIndex, data);
+
+      $timeout(function() {
+        $scope.notify = {};
+      }, 3500);
+    }, function(data){
+      $scope.notify = {
+        message: 'Error: ' + data,
+        class: 'bg-danger'
+      };
     });
   };
 
   $scope.createCampaign = function () {
+    $scope.notify = {
+      message: 'Saving...',
+      class: 'bg-warning'
+    };
     manageApi.call('post', 'campaigns', $scope.activeCampaign, function(data){
-      console.log(data);
+      $scope.myCampaigns.push(data);
+
+      $scope.notify = {
+        message: 'Campaign created.',
+        class: 'bg-success'
+      };
+    }, function(data){
+      $scope.notify = {
+        message: 'Error: ' + data,
+        class: 'bg-danger'
+      };
     });
   };
 
