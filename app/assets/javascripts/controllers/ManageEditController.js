9db21f9a64cb167096379139e6405ff6a5c3f8d7
adammeyer <adam.meyer@cru.org>
2014-08-04 14:57:02 -0400

    Ability to send email invites

--- app/assets/javascripts/controllers/ManageEditController.js
+++ app/assets/javascripts/controllers/ManageEditController.js
@@ -59,13 +59,29 @@ angular.module('chatApp').controller('ManageEditController', function ($scope, $
       message: 'Saving...',
       class: 'bg-warning'
     };
+    var inviteEmails = $scope.inviteEmails.split(',');
+
     manageApi.call('post', 'campaigns', $scope.activeCampaign, function(data) {
       $scope.myCampaigns.push(data);
 
-      $scope.notify = {
-        message: 'Campaign created.',
-        class: 'bg-success'
-      };
+      if(inviteEmails.length){
+          $scope.notify = {
+              message: 'Campaign created. Sending emails...',
+              class: 'bg-warning'
+          };
+
+          sendEmailInvite(inviteEmails, function(){
+              $scope.notify = {
+                  message: 'Campaign created. Emails Sent.',
+                  class: 'bg-success'
+              };
+          });
+      }else{
+          $scope.notify = {
+              message: 'Campaign created.',
+              class: 'bg-success'
+          };
+      }
 
       $location.path('/manage/' + data.uid);
     }, function(data){
@@ -74,6 +90,31 @@ angular.module('chatApp').controller('ManageEditController', function ($scope, $
         class: 'bg-danger'
       };
     });
+  };
+
+  var sendEmailInvite = function(inviteEmails, callback){
+      var to = inviteEmails[0];
+      manageApi.call('post', 'emails', {
+          to: to.trim(),
+          from: "noreply@watchthinkchat.com",
+          from_name: "WatchThinkChat",
+          message: "You have been invited to participate in '" + $scope.activeCampaign.title + "'. To access the operator console, go to http://www.godchat.tv/" + $scope.activeCampaign.permalink + ".\n\nPassword: " + $scope.activeCampaign.password,
+          subject: "Invite to join '" + $scope.activeCampaign.title + "'"
+      }, function() {
+          inviteEmails.shift();
+          if(inviteEmails.length){
+              sendEmailInvite(inviteEmails, callback);
+          }else{
+              callback();
+          }
+      }, function(){
+          inviteEmails.shift();
+          if(inviteEmails.length){
+              sendEmailInvite(inviteEmails, callback);
+          }else{
+              callback();
+          }
+      });
   };
 
   $scope.onlineOperatorCount = function() {
