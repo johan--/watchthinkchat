9a3ae33bef8caa3ef8e38b46e663498fae2b2409
Andrew Roth <andrewroth@gmail.com>
2014-08-06 12:52:43 -0400

    move the missionhub campaign create to create hook
    use the pattern where the create hook is in a subclass, so that I can create campaigns from the console without it being put into MH

---
+++ app/models/campaign/as_create.rb
@@ -0,0 +1,12 @@
+class Campaign::AsCreate < Campaign
+
+  after_create :create_missionhub_org
+
+  private
+
+  def create_missionhub_org
+    r = Rest.post("https://www.missionhub.com/apis/v3/organizations.json?secret=#{Rails.application.secrets.missionhub_token}&organization[name]=#{name}&organization[terminology]=Organization&organization[show_sub_orgs]=true&organization[status]=active&organization[parent_id]=#{Rails.application.secrets.missionhub_parent}&include=token")
+    # example return: {"organization":{"id":9747,"name":"New Org","terminology":"Organization","ancestry":"8349","show_sub_orgs":true,"status":"active","created_at":"2014-08-06T10:53:21-05:00","updated_at":"2014-08-06T10:53:21-05:00","token":"12345"}}
+    update_column(:missionhub_token, r["organization"]["token"])
+  end
+end
