2b0fcef74537e8b77d7ed2ce4aeacf565f81d26b
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-08-14 14:38:17 +1200

    splitting up assets from front-end and dashboard

---
+++ app/assets/javascripts/application/application.js
@@ -0,0 +1,8 @@
+//= require angular
+//= require angular-route
+//= require angular-ui-bootstrap-tpls
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require 'application/site'
+//= require_tree
