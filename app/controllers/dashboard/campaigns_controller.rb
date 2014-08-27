46091f1613ef1412217abef6b0486384327a0dd3
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-08-27 14:22:11 +1200

    updated campaigns controller to new format

--- app/controllers/dashboard/campaigns_controller.rb
+++ app/controllers/dashboard/campaigns_controller.rb
@@ -1,10 +1,41 @@
 module Dashboard
-  class CampaignsController < Dashboard::BaseResourceController
+  class CampaignsController < Dashboard::BaseController
+    def index
+      load_campaigns
+    end
+
     def new
-      @campaign = Campaign.create
-      @campaign.permissions.create(user: current_user,
-                                   state: Permission.states[:owner])
+      build_campaign
+      save_campaign
       redirect_to campaign_build_path(id: :basic, campaign_id: @campaign.id)
+    end
+
+    protected
+
+    def load_campaigns
+      @campaigns ||= campaign_scope
+    end
+
+    def campaign_scope
+      current_user.campaigns
+    end
+
+    def build_campaign
+      @campaign ||= campaign_scope.build
+      @campaign.attributes = campaign_params
+    end
+
+    def save_campaign
+      @campaign.save
+    end
+
+    def campaign_params
+      campaign_params = params[:campaign]
+      if campaign_params
+        campaign_params.permit(:title, :conditional)
+      else
+        {}
+      end
     end
   end
 end
