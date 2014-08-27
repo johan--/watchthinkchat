6d31ae7b637f4b6716d5c1d97057c29c1e404467
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-08-27 18:20:57 +1200

    enable backend to receive options through question model

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,12 +2,12 @@ class Option < ActiveRecord::Base
   enum conditional: [:next, :skip, :finish]
   belongs_to :question
   has_one :conditional_question, class: 'Question'
-  validates_presence_of :title, :question, :conditional
-  validates_presence_of :code, on: :update
-  validates_presence_of :conditional_question,
+  validates_presence_of :title, :conditional
+  validates_presence_of :question, on: :update
+  validates_presence_of :conditional_question_id,
                         if: proc { self.skip? }
   after_save :generate_code, on: :create
-  default_scope { order('created_at DESC') }
+  default_scope { order('created_at ASC') }
 
   protected
 
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -6,5 +6,10 @@ FactoryGirl.define do
     title { Faker::Lorem.sentence }
     help_text { Faker::Hacker.say_something_smart }
     sequence :position, 0
+    factory :question_with_options do
+      after(:create) do |question|
+        create_list(:option, 5, question: question)
+      end
+    end
   end
 end
