7e2acb3afbbee3a6f847a9493dd77ae960653150
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-05 14:32:11 +1200

    add email functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,12 +91,17 @@ Rails.application.configure do
 
   # smtp
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: ENV['smtp_user_name'],
-    password: ENV['smtp_password'],
-    address: 'email-smtp.us-east-1.amazonaws.com',
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    port: 587
+  config.action_mailer.default_url_options =
+    { host: "#{ENV['dashboard_url']}" }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
+
 end
