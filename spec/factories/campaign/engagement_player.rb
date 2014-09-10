cdb35c2de8635f3b3d153edbb8c5187eb7e4c702
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-10 22:50:06 +1200

    add start & stop to engagement_player

--- spec/factories/campaign/engagement_player.rb
+++ spec/factories/campaign/engagement_player.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :engagement_player, class: Campaign::EngagementPlayer do
     campaign
     media_link { Faker::Internet.url('youtube.com') }
+    media_start 0
+    media_stop 150
   end
 end
