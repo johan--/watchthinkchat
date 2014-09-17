2a3495de5d647e3ec95ab2b6ea8f2a84cb7e71af
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-17 13:38:47 +1200

    asset compilation and ignore travis files

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -4,9 +4,11 @@ Rails.application.config.after_initialize do
   ActiveSupport.on_load(:active_record) do
     if Rails.application.config.database_configuration
       config = Rails.application.config.database_configuration[Rails.env]
-      config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-      config['pool']              = ENV['DB_POOL']      || 5
-      ActiveRecord::Base.establish_connection(config)
+      if config['reaping_frequency'] && config['pool']
+        config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10
+        config['pool']              = ENV['DB_POOL']      || 5
+        ActiveRecord::Base.establish_connection(config)
+      end
     end
   end
 end
