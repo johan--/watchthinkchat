abdd78bf017ef8e235b173d3f44e6923e5c44839
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-19 10:13:06 +1200

    fetch proper campaign object for visitor

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,50 +1,14 @@
 class SiteController < ApplicationController
   def index
-    @campaign = OpenStruct.new(
-      name: '#FallingPlates',
-      engagement_player: {
-        media_link: 'https://www.youtube.com/watch?v=KGlx11BxF24',
-        questions: [
-          {
-            id: 0,
-            title: 'Jesus asks you, "Will you follow me?"',
-            help_text: '(Click one now)',
-            options: [
-              {
-                id: 0,
-                title: 'No thanks',
-                code: '98HbZKK',
-                conditional: 0
-              }, {
-                id: 1,
-                title: 'I follow another faith',
-                code: 'bUbkj56V',
-                conditional: 0
-              }, {
-                id: 2,
-                title: 'I am not sure',
-                code: 'AklnAK',
-                conditional: 0
-              }, {
-                id: 3,
-                title: 'I want to start',
-                code: 'PB3ave',
-                conditional: 0
-              }, {
-                id: 4,
-                title: 'I\'m trying',
-                code: '84rehrB',
-                conditional: 0
-              }, {
-                id: 5,
-                title: 'I\'m following Jesus',
-                code: 'Nkl397r',
-                conditional: 0
-              }
-            ]
-          }
-        ]
-      }
-    )
+    load_campaign
+    return render 'no_campaign' if @campaign.nil?
+  end
+
+  protected
+
+  def load_campaign
+    url = request.host
+    url.slice! ".#{ENV['base_url']}" if request.host.include? ENV['base_url']
+    @campaign = Campaign.opened.find_by(url: url)
   end
 end
