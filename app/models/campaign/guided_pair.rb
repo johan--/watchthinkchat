3d55c7590cbb7da7eadb80af9489bcf8105e3856
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-01 10:25:11 +1300

    add translatable to guided pair

--- app/models/campaign/guided_pair.rb
+++ app/models/campaign/guided_pair.rb
@@ -1,12 +1,16 @@
 class Campaign
   class GuidedPair < ActiveRecord::Base
+    extend Translatable
     # associations
     belongs_to :campaign
+    has_many :translations, as: :resource, dependent: :destroy
 
     # validations
     validates :campaign, presence: true
     validates :enabled, inclusion: [true, false]
 
+    # definitions
+    translatable :title, :description
     def title
       self[:title] ||
         I18n.t(:title, scope: [:models, :campaign, :guided_pair])
