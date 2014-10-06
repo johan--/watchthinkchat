8eae9eba8f4bcbb48321a5173ddd7f0c9bb3d111
adammeyer <adam.meyer@cru.org>
2014-10-06 13:08:19 -0400

    Ability to send email invites

--- app/assets/javascripts/application/controllers/old/ManageController.js
+++
@@ -1,5 +0,0 @@
-angular.module('chatApp').controller('ManageController', function ($scope, manageApi) {
-  manageApi.call('get', 'campaigns', {}, function(data){
-    $scope.myCampaigns = data;
-  }, null, true);
-});
