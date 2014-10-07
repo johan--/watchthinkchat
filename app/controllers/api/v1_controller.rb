c3c34d5296809260e57750d6a8939efb190b821b
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-08 12:00:28 +1300

    set max line length to 120

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -7,12 +7,10 @@ module Api
     protected
 
     def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] =
-        '*'
+      headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] =
         'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] =
-        '*'
+      headers['Access-Control-Request-Method'] = '*'
       headers['Access-Control-Allow-Headers'] =
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
