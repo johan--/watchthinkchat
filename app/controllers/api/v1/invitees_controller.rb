4079ded477a08b4db9def2cbb9d4fb9b058b4702
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-15 17:25:41 +1300

    invitees api endpoint

---
+++ app/controllers/api/v1/invitees_controller.rb
@@ -0,0 +1,67 @@
+module Api
+  module V1
+    class InviteesController < Api::V1Controller
+      def index
+        load_invitees
+      end
+
+      def show
+        load_invitee
+      rescue ActiveRecord::RecordNotFound => ex
+        render json: { errors: ex.message }.to_json,
+               status: :not_found
+      end
+
+      def create
+        build_invitee
+        save_invitee
+        render 'show', status: :created
+      rescue ArgumentError, ActiveRecord::RecordInvalid => ex
+        render json: { errors: ex.message }.to_json,
+               status: :unprocessable_entity
+      end
+
+      def update
+        load_invitee
+        build_invitee
+        save_invitee
+        render 'show', status: :ok
+      rescue ArgumentError, ActiveRecord::RecordInvalid => ex
+        render json: { errors: ex.message }.to_json,
+               status: :unprocessable_entity
+      end
+
+      protected
+
+      def load_invitees
+        @invitees ||= invitee_scope
+      end
+
+      def load_invitee
+        @invitee ||= invitee_scope.find(params[:id])
+        @invitee
+      end
+
+      def build_invitee
+        @invitee ||= invitee_scope.build
+        @invitee.attributes = invitee_params
+      end
+
+      def save_invitee
+        @invitee.save!
+      end
+
+      def invitee_scope
+        current_visitor.invitees
+      end
+
+      def invitee_params
+        invitee_params = params[:invitee]
+        return {} unless invitee_params
+        invitee_params.permit(:first_name,
+                              :last_name,
+                              :email)
+      end
+    end
+  end
+end
---
+++ app/views/api/v1/invitees/show.rabl
@@ -0,0 +1,2 @@
+object @invitee
+attributes :id, :first_name, :last_name, :email, :invite_token
