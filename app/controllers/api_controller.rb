0104595a77a115bacb09fc5b408c16c9a39908bf
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-16 15:05:36 +1300

    load correct visitor object when visitor has invite_token

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,9 +4,17 @@ class ApiController < ApplicationController
 
   def token
     unless user_signed_in?
-      visitor = Visitor.create
+      visitor = load_visitor
       sign_in visitor
     end
     @token = current_visitor.authentication_token
+  end
+
+  private
+
+  def load_visitor
+    invite = URI(request.referer).path.match(%r{/i/(?<token>.*)/?})
+    return Visitor.create if invite.nil?
+    Visitor::Invitation.find_by(token: invite[:token]).invitee
   end
 end
--- spec/requests/api_controller_spec.rb
+++ spec/requests/api_controller_spec.rb
@@ -4,14 +4,32 @@ RSpec.describe ApiController, type: :request do
   let(:campaign) { create(:campaign) }
 
   describe 'token' do
-    context 'arbitrary visitor' do
+    context 'visitor from unknown' do
       it 'returns access token' do
         expect do
           get "http://api.#{ENV['base_url']}/token.js",
+              {},
               referer: "http://#{campaign.url}/"
         end.to change(Visitor, :count).by(1)
         expect(response.body[/\".*?\"/].gsub(/"/, '')).to(
           eq(Visitor.first.authentication_token))
+      end
+    end
+    context 'visitor from invite token' do
+      it 'returns access token of invitee' do
+        @inviter = create(:inviter)
+        @invitee = create(:invitee)
+        @invitation =  create(:invitation,
+                              invitee: @invitee,
+                              inviter: @inviter,
+                              campaign_id: campaign.id)
+        expect do
+          get "http://api.#{ENV['base_url']}/token.js",
+              {},
+              referer: "http://#{campaign.url}/i/#{@invitation.token}"
+        end.to change(Visitor, :count).by(0)
+        expect(response.body[/\".*?\"/].gsub(/"/, '')).to(
+          eq(@invitee.authentication_token))
       end
     end
   end
