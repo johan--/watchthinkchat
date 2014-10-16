3196f741887ca7d51ecc12bcfb234b55f2e66eea
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-17 06:20:29 +1300

    add header authentication and render_unauthorised

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,15 +1,31 @@
 module Api
   class V1Controller < ApiController
     before_action :cors_set_access_control_headers
-    before_action :authenticate_user_from_token!
+    before_action :authenticate
     before_action :load_campaign
 
     protected
 
-    def authenticate_user_from_token!
-      user_token = params[:access_token].presence
-      user = user_token && Visitor.find_by(authentication_token: user_token.to_s)
-      sign_in user, store: false if user
+    def authenticate
+      authenticate_header_token || authenticate_param_token || render_unauthorized
+    end
+
+    def authenticate_header_token
+      authenticate_with_http_token do |token, _options|
+        sign_in Visitor.find_by(authentication_token: token)
+      end
+    end
+
+    def authenticate_param_token
+      token = params[:access_token].presence
+      sign_in(token && Visitor.find_by(authentication_token: token.to_s), store: false)
+    rescue
+      false
+    end
+
+    def render_unauthorized
+      headers['WWW-Authenticate'] = 'Token realm="Application"'
+      render json: 'Bad credentials', status: 401
     end
 
     def cors_set_access_control_headers
