823835cd95d77c17d362b5211609f2f7d11f39b5
adammeyer <adam.meyer@cru.org>
2014-10-22 11:20:10 -0400

    invite pair beginnings

--- app/assets/javascripts/application/services/api.js
+++ app/assets/javascripts/application/services/api.js
@@ -24,4 +24,27 @@ angular.module('chatApp').service('api', function ($http, $cacheFactory) {
           }
         });
   };
+
+  this.call = function (method, url, data, successFn, errorFn) {
+    $http({
+      method: method,
+      url: apiUrl + url,
+      data: data,
+      headers: {
+        Authorization: 'Token token="' + window.token + '"'
+      },
+      timeout: 5000
+    }).
+        success(function(data, status) {
+          if(_.isFunction(successFn)){
+            successFn(data, status);
+          }
+        }).
+        error(function(data, status) {
+          console.log('API ERROR: ' + status);
+          if(_.isFunction(errorFn)){
+            errorFn(data, status);
+          }
+        });
+  };
 });
