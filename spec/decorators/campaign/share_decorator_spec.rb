1c9c7d2d6d72cce4166f3a83c4e698cc986ac138
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-10-23 15:15:30 +1300

    THINK-268 THINK-270 THINK-271 THINK-305 THINK-307 THINK-308 add subject and message to share build_controller, yml, visitor obj

--- spec/decorators/campaign/share_decorator_spec.rb
+++ spec/decorators/campaign/share_decorator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Campaign::ShareDecorator, type: :decorator do
   let(:share) do
-    create(:share, title: '', description: '').decorate
+    create(:share, title: '', description: '', subject: '', message: '').decorate
   end
 
   it 'returns default title if not present' do
@@ -12,5 +12,13 @@ describe Campaign::ShareDecorator, type: :decorator do
   it 'returns default description if not present' do
     expect(share.description)
       .to eq(I18n.t :description, scope: [:models, :campaign, :share])
+  end
+  it 'returns default subject if not present' do
+    expect(share.subject)
+      .to eq(I18n.t :subject, scope: [:models, :campaign, :share])
+  end
+  it 'returns default message if not present' do
+    expect(share.message)
+      .to eq(I18n.t :message, scope: [:models, :campaign, :share])
   end
 end
